4fe9a7b7a9fc9db9d6f1aa0989614ec36ee57a0c
Alexander Sanchez <pcua2014@hotmail.com>
2021-09-08 17:22:27 -0400

    Main App Setup with MongoDB

---
+++ Main/src/app.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response} from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { createConnection } from 'typeorm';
+
+createConnection().then(db =>  {
+    dotenv.config();
+    const PORT = process.env.PORT || 3300;
+    
+    const app = express();
+    
+    app.use(cors());
+    app.use(express.json());
+
+    app.listen(PORT, () => {
+        console.log(`Running server in port ${PORT}`);
+    });
+})
