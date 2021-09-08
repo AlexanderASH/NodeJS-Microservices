d4f1bd08e03c094b9d5875809db1ddc28c6ac4d7
Alexander Sanchez <pcua2014@hotmail.com>
2021-09-07 22:38:58 -0400

    connecting with PostgreSQL

--- Admin/src/app.ts
+++ Admin/src/app.ts
@@ -1,15 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { createConnection } from 'typeorm';
 
-dotenv.config();
-const PORT = process.env.PORT || 3300;
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.listen(PORT, () => {
-    console.log(`Running server in port ${PORT}`);
-});
+createConnection().then(db =>  {
+    dotenv.config();
+    const PORT = process.env.PORT || 3300;
+    
+    const app = express();
+    
+    app.use(cors());
+    app.use(express.json());
+    
+    app.listen(PORT, () => {
+        console.log(`Running server in port ${PORT}`);
+    });
+})
